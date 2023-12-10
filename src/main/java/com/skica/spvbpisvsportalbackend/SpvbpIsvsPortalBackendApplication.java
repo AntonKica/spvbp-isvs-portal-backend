e0181f73c91bee5b674a83cf46d3fe974d76d25b
akica <akica@gratex.com>
2023-12-10 19:00:59 +0100

    Initialized Java gradle project

---
+++ src/main/java/com/skica/spvbpisvsportalbackend/SpvbpIsvsPortalBackendApplication.java
@@ -0,0 +1,12 @@
+package com.skica.spvbpisvsportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpvbpIsvsPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpvbpIsvsPortalBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/skica/spvbpisvsportalbackend/SpvbpIsvsPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skica.spvbpisvsportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpvbpIsvsPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
