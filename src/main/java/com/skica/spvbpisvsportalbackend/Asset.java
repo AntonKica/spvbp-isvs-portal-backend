1413a46db85d47c0840edd7451b319aac3047071
akica <akica@gratex.com>
2023-12-12 09:11:30 +0100

    Simple asset controller

--- src/main/java/com/skica/spvbpisvsportalbackend/Asset.java
+++ src/main/java/com/skica/spvbpisvsportalbackend/Asset.java
@@ -1,10 +1,12 @@
 package com.skica.spvbpisvsportalbackend;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 
 @Entity
 @Table(name="assets")
 public class Asset {
+    @Nullable
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public long id;
--- src/main/java/com/skica/spvbpisvsportalbackend/SimpleController.java
+++ src/main/java/com/skica/spvbpisvsportalbackend/SimpleController.java
@@ -7,10 +7,14 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -39,15 +43,50 @@ public class SimpleController implements InitializingBean {
         tx.commit();
     }
 
+    @PostMapping(value = "/asset", consumes = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity create(@RequestBody Asset asset) {
+        Session session = getSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(asset);
+        tx.commit();
+        session.close();
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(asset.id)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
     @GetMapping(value = "/asset/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    Asset getHello(@PathVariable long id) {
+    ResponseEntity<Asset> getAsset(@PathVariable long id) {
         var session = getSession();
         var asset =  session.get(Asset.class, id);
         session.close();
-        return asset;
+        if(asset != null)
+            return ResponseEntity.ok(asset);
+        else
+            return ResponseEntity.notFound().build();
     }
 
-    @GetMapping(value = "/asset", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/asset/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity update(@PathVariable long id, @RequestBody Asset asset) {
+        Session session = getSession();
+        Transaction tx = session.beginTransaction();
+        Asset old = session.get(Asset.class, id);
+        if(old == null) {
+            return ResponseEntity.notFound().build();
+        }
+        session.merge(asset);
+        tx.commit();
+        session.close();
+
+        return ResponseEntity.ok(Response.SC_OK);
+    }
+
+    @GetMapping(value = "/list-assets", produces = MediaType.APPLICATION_JSON_VALUE)
     List<Asset> getHello() {
         var session = getSession();
         var query = session.getCriteriaBuilder().createQuery(Asset.class);
@@ -58,20 +97,7 @@ public class SimpleController implements InitializingBean {
         return res;
     }
 
-    @PostMapping(value = "/asset", produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity update(@RequestBody Asset asset) {
-        Session session = getSession();
-        Transaction tx = session.beginTransaction();
-        Asset old = session.get(Asset.class, asset.id);
-        if(old == null) {
-            return ResponseEntity.status(Response.SC_NOT_FOUND).body("Not found");
-        }
-        session.merge(asset);
-        tx.commit();
-        session.close();
 
-        return ResponseEntity.ok(Response.SC_OK);
-    }
 
 }
 
